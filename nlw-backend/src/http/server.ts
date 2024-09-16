feaa4c6930becf7f78e4714460720c336bdc5993
Lucas Pilati <pilatilucas@gmail.com>
2024-09-15 23:06:55 -0300

    adjustment in backend

--- nlw-backend/src/http/server.ts
+++ nlw-backend/src/http/server.ts
@@ -1,6 +1,28 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/createGoal'
+import { completionGoalRoute } from './routes/completionGoal'
+import { getPedingGoalsRoute } from './routes/getPedingGoals'
+import { getWeekSummaryRoute } from './routes/getWeekSummary'
+import fastifyCors from '@fastify/cors'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(completionGoalRoute)
+app.register(getPedingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
@@ -8,4 +30,4 @@ app
   })
   .then(() => {
     console.log('HTTPS SERVER')
-  });
+  })
