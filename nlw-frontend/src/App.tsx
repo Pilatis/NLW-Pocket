81dddbf6f5100e80ec1bf946191faf1336cd5084
Lucas Pilati <pilatilucas@gmail.com>
2024-09-15 22:26:59 -0300

    initial frontend

---
+++ nlw-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Container, TextField, DialogContentText, Box } from "@mui/material";
+import Homepage from "./components/Homepage";
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Homepage />
+    </Container>
+  )
+}
+
+export default App;
---
+++ nlw-frontend/src/components/Homepage/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <Box>
+  
+    </Box>
+  )
+}
+
+export default Homepage
